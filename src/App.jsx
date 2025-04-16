dd5a466ed229a071d0fe0977e58daba7280aeb9f
unknown <bd.in.jh@gmail.com>
2025-04-16 11:07:37 +0600

    Project Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex flex-col mt-40'>
+        <button className='btn btn-primary btn-block'>PH Module 44 Practice Project Basic Setup</button>
+      </div>
+    </>
+  )
+}
+
+export default App
